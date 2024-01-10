8f0b9bb93486251c586f2dd1d03aeed5616f4642
nikolp2 <drakko1988@gmail.com>
2024-01-10 12:18:57 -0300

    spring1.5

--- app.js
+++ app.js
@@ -1,14 +1,83 @@
-let cards = document.getElementById('cardConteiner');
-movies.map((x) => {
-    cardConteiner.innerHTML += `
-      <div>
-        <div class="card border-slate-100 w-40 p-1 overflow-auto text-'read more' h-64">
-          <img src="${x.image}" class="card-img" alt="">
-          <div class="card-body">
-            <h5 class="card-title my-3 text-center text-lg font-semibold">${x.title}</h5>
-            <p class="card-text italic">${x.overview}</p>
-          </div>
-        </div>
-      </div>
-    `;
+const cards = document.getElementById('cardConteiner');
+const getList = document.getElementById('inputSearch')
+const genreFilter = document.getElementById('genres')
+
+function createCard(movie) {
+  return `
+    <div class="card border-slate-100 w-40 p-1 overflow-auto text-'read more' h-64 flex-col justify-between">
+      <img src="${movie.image}" class="card-img" alt="">
+      <div class="card-body align-center justify-center">
+        <h5 class="card-title my-3 text-center text-lg font-semibold">${movie.title}</h5>
+        <p class="italic footerText my-4 justify-center">${movie.tagline}</p>
+        <a class="btn align-center justify-center m-6" href="./description.html?id=${movie.id}"><button type="button">Read more</button></a>
+      </div>          
+    </div>
+  `;
+}
+
+//card creator
+renderCards(movies);
+function renderCards(filteredMovies) {
+  cards.innerHTML = '';
+  filteredMovies.forEach(movie => {
+    cards.innerHTML += createCard(movie);
   });
+}
+//--------------------------------------
+
+
+//genre search
+genres.addEventListener('change', function() {
+  const selectedGenre = genres.value;
+  let filteredMovies;
+
+  if (selectedGenre == 'All') {
+    filteredMovies = movies;
+  } else {
+    filteredMovies = movies.filter(movie => movie.genres.includes(selectedGenre));
+  }
+
+  renderCards(filteredMovies);
+});
+//--------------------------------------
+
+
+
+
+//list creator
+const genresList = getGenres(movies);
+
+genreFilter.innerHTML = genresList.reduce((accumulator, genre)=> accumulator += createGenres(genre), '<option selected>All</option>');
+function createGenres(genre) {
+  return `<option value="${genre}">${genre}</option>`
+}
+//--------------------------------------
+
+
+
+//input search
+getList.addEventListener('input', () => {
+  const selectedInput = filterList(movies, getList.value);
+  if (filterList == "") {    
+    renderCards(movies);
+  } else {
+    renderCards(selectedInput);
+  }
+})
+
+function filterList(movies, query){
+  movies.filter( (movie) => {
+    return movie.title.toLowerCase().includes(query.toLowerCase())
+  })
+}
+//--------------------------------------
+
+
+
+function getGenres(movies) {
+  const moviesGenres = movies
+  .map(movie => movie.genres)
+  .flat()
+  .filter((genre, index, genres) => genres.indexOf(genre) == index);
+  return moviesGenres    
+}
---
+++ details.js
@@ -0,0 +1,59 @@
+const imageSlot = document.getElementById("imageSlot")
+const detailSlot = document.getElementById("detailSlot")
+const moneySlot = document.getElementById("moneySlot")
+const statusSlot = document.getElementById("statusSlot")
+
+const queryParams = new URLSearchParams(window.location.search);
+
+const id = queryParams.get("id")
+
+const movie = movies.find(movie => movie.id == id)
+
+console.log(movies)
+
+imageSlot.innerHTML = `<img src="${movie.image}" alt="${movie.title}">`
+
+detailSlot.innerHTML = `
+        <h2 class=" text-3xl">${movie.title}</h2>;
+        <p>${movie.tagline}</p>
+        <p>${movie.genres}</p>
+        <p>${movie.overview}</p>
+`
+
+statusSlot.innerHTML = `
+        <table class="border-slate-50">
+            <tr>
+                <td>Original Language</td>
+                <td>${movie.original_language}</td>
+            </tr>
+            <tr>
+                <td>Release Date</td>
+                <td>${movie.release_date}</td>
+            </tr>
+            <tr>
+                <td>Runtime</td>
+                <td>${movie.runtime}</td>
+            </tr>
+            <tr>
+                <td>Status</td>
+                <td>${movie.status}</td>
+            </tr>
+        </table>    
+`
+
+moneySlot.innerHTML = `
+        <table>
+            <tr>
+                <td>Vote Average</td>
+                <td>${movie.vote_average}</td>
+            </tr>
+            <tr>
+                <td>Budget</td>
+                <td>$ ${movie.budget.toLocaleString('en-US')}</td>
+            </tr>
+            <tr>
+                <td>revenue</td>
+                <td>$ ${movie.revenue.toLocaleString('en-US')}</td>
+            </tr>
+        </table>
+`
